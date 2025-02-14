6bde90831610f985d5d93290e69177c1bd4c9099
Mrabet Adem <adem.mrabet99@gmail.com>
2025-02-14 10:14:05 +0100

    error handling : v3

---
+++ server/models/Brands.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const brandSchema = new mongoose.Schema({
+    brandId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    brandName: {
+        type: String,
+        required: true,
+        unique: true, // Ensure brand names are unique
+    },
+    logo: {
+        type: String, // Single URL for the logo (unless you need multiple logos)
+        required: true,
+    },
+    description: {
+        type: String, // Optional description for the brand
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now, // Automatically record when the brand was created
+    },
+});
+
+// Create the Brand model
+const Brand = mongoose.model('Brand', brandSchema);
+
+// Export the Brand model
+module.exports = Brand;
