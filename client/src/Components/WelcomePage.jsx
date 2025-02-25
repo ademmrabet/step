696ec036de2e8d0404007edf77a512c00ae9fe38
Mrabet Adem <adem.mrabet99@gmail.com>
2025-02-25 19:13:50 +0100

    fixing order Bug 1.0

---
+++ client/src/Components/WelcomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const WelcomePage = () => {
+  return (
+    <div className=''>
+        <div className='display-flex justify-center flex-row font-inter text-white bg-black'>
+            <div className='font-family-inter'>
+                <h1>Step Footwear</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default WelcomePage
