f1f11345dddbeb57ff381d6f8586d19d12cca79c
Mrabet Adem <adem.mrabet99@gmail.com>
2025-02-25 11:01:38 +0100

    idk what i changed

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const orderController = require('../controllers/orderController.js')
+const orderController = require('../controllers/orderController');
 
 router.post('/', orderController.createOrder);
 router.get('/', orderController.getAllOrders);
